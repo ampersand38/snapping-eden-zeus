4e482740968a03c171d4c1e0377451cf53e6a9f1
Steve Zhao <ampersand38@gmail.com>
2025-03-17 18:30:28 -0400

    Fix ACE overwrite Land_Razorwire_F
    close #6

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -1,4 +1,5 @@
 class EventHandlers;
+class CBA_Extended_EventHandlers;
 class CfgVehicles
 {
     class All;
@@ -6,8 +7,22 @@ class CfgVehicles
     {
         class EventHandlers: EventHandlers
         {
-            dragged3DEN = "_this call sez_main_fnc_snap;";
-            registeredToWorld3DEN = "_this call sez_main_fnc_snap;";
+            class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers
+            {
+                dragged3DEN = "_this call sez_main_fnc_snap;";
+                registeredToWorld3DEN = "_this call sez_main_fnc_snap;";
+            };
+        };
+    };
+
+    // Fix ACE overwrite https://github.com/acemod/ACE3/blob/master/addons/concertina_wire/CfgVehicles.hpp#L150
+    class NonStrategic;
+    class Land_Razorwire_F: NonStrategic {
+        class EventHandlers: EventHandlers {
+            class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {
+                dragged3DEN = "_this call sez_main_fnc_snap;";
+                registeredToWorld3DEN = "_this call sez_main_fnc_snap;";
+            };
         };
     };
 }; // CfgVehicles
